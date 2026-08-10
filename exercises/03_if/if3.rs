a294cf1a68692fe8e6ba9b148eaefda7e23943f0
Barnabas Enwemasor <qhodeweb@gmail.com>
2026-08-10 23:22:03 +0100

    if statement exercise completed

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -1,21 +1,21 @@
 fn animal_habitat(animal: &str) -> &str {
     // TODO: Fix the compiler error in the statement below.
-    let identifier = if animal == "crab" {
-        1
+    let identifier: f64 = if animal == "crab" {
+        1.0
     } else if animal == "gopher" {
         2.0
     } else if animal == "snake" {
-        3
+        3.0
     } else {
-        "Unknown"
+        4.0
     };
 
     // Don't change the expression below!
-    if identifier == 1 {
+    if identifier == 1.0 {
         "Beach"
-    } else if identifier == 2 {
+    } else if identifier == 2.0 {
         "Burrow"
-    } else if identifier == 3 {
+    } else if identifier == 3.0 {
         "Desert"
     } else {
         "Unknown"
